96caa282456bcab5859e5891bf93039c94a25c0b
krishna <krishna.bose02@gmail.com>
2020-07-16 04:24:42 +0530

    Embedded barcode scanner working

--- app/src/main/java/com/joblesscoders/arbook/qrscan/QRScanActivity.java
+++ app/src/main/java/com/joblesscoders/arbook/qrscan/QRScanActivity.java
@@ -5,39 +5,92 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.joblesscoders.arbook.R;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class QRScanActivity extends AppCompatActivity {
+final String TAG = "QRScanActivity";
+DecoratedBarcodeView bcv;
+TextView displayData;
+
+private BarcodeCallback callback = result -> {
+    Toast.makeText(QRScanActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+    Log.e(TAG, "barcodeResult: " + result.getText());
+    bcv.getBarcodeView().stopDecoding();
+    displayData.setText(result.getText());
+    bcv.setVisibility(View.GONE);
+};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
+        bcv = (DecoratedBarcodeView) findViewById(R.id.decorated_barcode_view);
+
+        // Go through the XML file to know which component it refers to
+        displayData = findViewById(R.id.qr_code_data);
+        displayData.setText("Nothing scanned yet");
     }
 
-    public void startScan(View v) {
-        new IntentIntegrator(this)
-                .setBarcodeImageEnabled(true)
-                .setBeepEnabled(true)
-                .initiateScan();
+    public void initScanner() {
+        bcv.setVisibility(View.VISIBLE);
+
+        // In case we want to limit the type of barcodes that can be scanned, we can use the following
+//        Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.UPC_A);
+
+        // List of decoder types, sending no params make it open to app valid code types
+        bcv.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory());
+
+        // God knows what it does, I copied this because it wasn't working before
+        bcv.initializeFromIntent(getIntent());
+
+        // This starts the final scanning action, until this is called, the camera may show contents, but won't scan anything
+        bcv.decodeSingle(callback);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bcv.resume();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+    protected void onPause() {
+        super.onPause();
+        bcv.pause();
     }
+
+    // This function is called when scan button is pressed
+    public void startScan(View v) {
+
+        // This is the default full screen activity, better to not use it
+//        new IntentIntegrator(this)
+//                .setOrientationLocked(true)
+//                .initiateScan();
+
+        if (bcv.getVisibility() == View.VISIBLE) {
+
+            // Scanner already being shown, no need to init it again
+            return;
+        }
+
+        initScanner();
+    }
+
 }
