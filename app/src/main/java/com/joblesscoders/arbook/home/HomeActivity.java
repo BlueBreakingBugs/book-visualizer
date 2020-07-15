3ee5bc5b5dfe9f4620dfc85cd560f2d8f228c8e5
krishna <krishna.bose02@gmail.com>
2020-07-15 23:28:41 +0530

    Added barcode scanner for new book addition

--- app/src/main/java/com/joblesscoders/arbook/home/HomeActivity.java
+++ app/src/main/java/com/joblesscoders/arbook/home/HomeActivity.java
@@ -2,9 +2,12 @@ package com.joblesscoders.arbook.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.joblesscoders.arbook.R;
+import com.joblesscoders.arbook.qrscan.QRScanActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +15,36 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fetchBookData();
+    }
+
+    public String[] getRegisteredBooks() {
+        // load list of books from SharedPreference
+        // TODO, parse the data from SharedPreference
+        return null;
+    }
+    public void fetchBookData() {
+        // load the list of books registered by the user
+        String books[] = getRegisteredBooks();
+
+        // call api and get details of these books
+        // TODO, do it.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fetchBookData();
+    }
+
+    public void goToScannerActivity(View v) {
+        // go to qr code scanner activity to add new book
+
+        // Intent object takes reference of current activity, and class of the destination activity
+        Intent intent = new Intent(this, QRScanActivity.class);
+
+        // Start calling startActivity with the intent loads the new activity on top of current stack
+        startActivity(intent);
+
+        // also in case you need to close an activity, you can call finish()
     }
 }
--- app/src/main/java/com/joblesscoders/arbook/qrscan/QRScanActivity.java
+++ app/src/main/java/com/joblesscoders/arbook/qrscan/QRScanActivity.java
@@ -1,9 +1,15 @@
 package com.joblesscoders.arbook.qrscan;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.joblesscoders.arbook.R;
 
 public class QRScanActivity extends AppCompatActivity {
@@ -12,5 +18,26 @@ public class QRScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
+    }
+
+    public void startScan(View v) {
+        new IntentIntegrator(this)
+                .setBarcodeImageEnabled(true)
+                .setBeepEnabled(true)
+                .initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
